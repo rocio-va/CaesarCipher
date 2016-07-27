2da9717e8cb65d3490a7f37f65693f7d2bdf3706
rocio-va <rociova94@gmail.com>
2016-07-27 13:02:52 +0200

    second iteration

--- solveCipher.rb
+++ solveCipher.rb
@@ -1,26 +1,33 @@
 require "pry"
 
-def solve_cipher(word)
-	word.downcase
+def solve_cipher(phrase, n)
+	phrase.downcase!
+	phrase.chomp(".")
 	#la función char, devuelve un array de caracteres
-	word_array = word.chars
+	word_array = phrase.chars
 	new_array = []
 	#"a".ord = 97 "z".ord = 122
 	word_array.each do |item|
-		if (item.ord == 97)
-			item = 122.chr
+		if (item == " ")
 			new_array.push item
+		elsif (item.ord + n < 97)
+			item = 123 + (item.ord + n - 97)
+			new_array.push item.chr
+		elsif (item.ord + n > 122)
+			item = 96 + (item.ord + n - 122)
+			new_array.push item.chr
 		else
-			item = (item.ord - 1).chr
-			new_array.push item
-		end
+			item = item.ord + n
+			new_array.push item.chr
+		end	
 	end
 	new_array.join
-
 end
 
-puts "Tell me one word."
-word = gets.chomp
-puts solve_cipher(word)
+puts "Tell me something."
+phrase = gets.chomp
+puts "Tell me one number."
+n = gets.chomp.to_i
+puts solve_cipher(phrase, n)
 
 binding.pry
